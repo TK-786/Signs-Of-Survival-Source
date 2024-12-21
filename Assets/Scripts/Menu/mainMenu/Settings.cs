1a5cf825e90d1f2a4af7b4ffb5b8a6e1428af58a
Hamid Hafiz <hamidhafiz3@outlook.com>
2024-12-21 16:27:49 +0000

    updated settings script for pause menu
    added button functionality

--- Assets/Scripts/Menu/mainMenu/Settings.cs
+++ Assets/Scripts/Menu/mainMenu/Settings.cs
@@ -4,37 +4,94 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 using TMPro;
+using UnityEditor.Search;
 
 public class Settings : MonoBehaviour
 {
-    public Slider volumeSlider;
-    public Slider brightnessSlider;
-    public Image brightnessOverlay;
+    //public Slider volumeSlider;
+    //public Slider brightnessSlider;
+    //public Image brightnessOverlay;
+
+    public GameObject basicSettings;
+    public GameObject keyBindsSettings;
+
+    public GameObject basicButton;
+    public GameObject keyBindsButton;
+
+    private Vector3 basicButtonOgPosition;
+    private Vector3 keyBindsButtonOgPosition;
+
+    public float popOutAmount = 20f; 
+    public float popOutSpeed = 5f;
+
     // Start is called before the first frame update
-    void Start()
+    //void Start()
+    //{
+    //    volumeSlider.value = AudioListener.volume;
+    //    volumeSlider.onValueChanged.AddListener(ChangeVolume);
+
+    //    brightnessSlider.value = PlayerPrefs.GetFloat("Brightness", 2f);
+    //    brightnessSlider.onValueChanged.AddListener(ChangeBrightnessFunc);
+
+    //}
+
+    //public void ChangeVolume(float v)
+    //{
+    //    AudioListener.volume = v;
+    //}
+
+    //public void ChangeBrightnessFunc(float b)
+    //{
+    //    PlayerPrefs.SetFloat("Brightness", b);
+    //    PlayerPrefs.Save();
+    //    Color color = brightnessOverlay.color;
+    //    color.a = 1 - b;
+    //    brightnessOverlay.color = color;
+
+
+    //}
+
+    private void Start()
     {
-        volumeSlider.value = AudioListener.volume;
-        volumeSlider.onValueChanged.AddListener(ChangeVolume);
-
-        brightnessSlider.value = PlayerPrefs.GetFloat("Brightness", 2f);
-        brightnessSlider.onValueChanged.AddListener(ChangeBrightnessFunc);
-
+        basicButtonOgPosition = basicButton.transform.localPosition;
+        keyBindsButtonOgPosition = keyBindsButton.transform.localPosition;
+        
     }
 
-    public void ChangeVolume(float v)
+    public void ShowBasicSettings()
     {
-        AudioListener.volume = v;
+        basicSettings.SetActive(true);
+        keyBindsSettings.SetActive(false);
+
+        buttonPopOut(basicButton);
+        ResetButton(keyBindsButton);
+
+
+    }
+    public void ShowKeyBindsSettings()
+    {
+        basicSettings.SetActive(false);
+        keyBindsSettings.SetActive(true);
+
+        buttonPopOut(keyBindsButton);
+        ResetButton(basicButton);
     }
 
-    public void ChangeBrightnessFunc(float b)
+    private void buttonPopOut(GameObject clickedButton)
     {
-        PlayerPrefs.SetFloat("Brightness", b);
-        PlayerPrefs.Save();
-        Color color = brightnessOverlay.color;
-        color.a = 1 - b;
-        brightnessOverlay.color = color;
+        if (clickedButton != null)
+        {
+            clickedButton.transform.localPosition += new Vector3(popOutAmount, 0, 0);
+        }
 
-
+    }
+    private void ResetButton(GameObject clickedButton)
+    {
+        if (clickedButton != null)
+        {
+            Vector3 targetPosition = clickedButton == basicButton ? basicButtonOgPosition : keyBindsButtonOgPosition;
+            clickedButton.transform.localPosition = Vector3.Lerp(clickedButton.transform.localPosition, targetPosition, Time.deltaTime * popOutSpeed);
+        }
     }
 
 }
